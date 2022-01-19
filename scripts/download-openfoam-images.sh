0c265fbee6832cf1b80dbc811b5ce01f42c5877e
jtorrex <jose.torres@hpcnow.com>
2022-01-19 13:20:59 +0100

    Modified container movement

--- scripts/download-openfoam-images.sh
+++ scripts/download-openfoam-images.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir -p ../singularity-images
 singularity pull library://fertinaz-hpc/openfoam/of-7.sif:sha256.87a06205d8f66a4d3c2391e1a8eed8358e85de63588682e398fa81eded65d417
-mv of-7.sif_sha256.87a06205d8f66a4d3c2391e1a8eed8358e85de63588682e398fa81eded65d417.sif of-7.sif
+mv of-7.sif_sha256.87a06205d8f66a4d3c2391e1a8eed8358e85de63588682e398fa81eded65d417.sif ../singularity-images/of-7.sif
